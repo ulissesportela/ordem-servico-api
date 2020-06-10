a0de9e8cc0481f7451a08e401358429dd10e52f5
Ulisses Douglas <ulisses.portela@gmail.com>
2020-06-09 23:12:07 -0300

    incluinto Data transfer Onject, e o ModelMapper na entidade OrdemServico

--- src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
+++ src/main/java/com/algaworks/osworks/api/controller/OrdemServicoController.java
@@ -2,9 +2,12 @@ package com.algaworks.osworks.api.controller;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 import javax.validation.Valid;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.osworks.api.DTO.OrdemServicoDTO;
 import com.algaworks.osworks.domain.model.OrdemServico;
 import com.algaworks.osworks.domain.repository.OrdemServicoRepository;
 import com.algaworks.osworks.domain.service.GestaoOrdemServicoService;
@@ -30,25 +34,37 @@ public class OrdemServicoController {
 	@Autowired
 	private OrdemServicoRepository ordemServicoRepository;
 	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico) {
-		return gestaoOrdemServico.criar(ordemServico);
+	public OrdemServicoDTO criar(@Valid @RequestBody OrdemServico ordemServico) {
+		return toModel(gestaoOrdemServico.criar(ordemServico));
 	}
 	
 	@GetMapping
-	public List<OrdemServico> list() {
-		return ordemServicoRepository.findAll();
+	public List<OrdemServicoDTO> list() {
+		return toCollectionDTO(ordemServicoRepository.findAll());
 	}
 	
 	@GetMapping("/{ordemServicoId}")
-	public ResponseEntity<OrdemServico> busca(@PathVariable Long ordemServicoId) {
+	public ResponseEntity<OrdemServicoDTO> busca(@PathVariable Long ordemServicoId) {
 		Optional<OrdemServico> ordemServico =  ordemServicoRepository.findById(ordemServicoId);
 		
 		if(ordemServico.isPresent()) {
-			return ResponseEntity.ok(ordemServico.get());
+			OrdemServicoDTO dto = toModel(ordemServico.get());
+			return ResponseEntity.ok(dto);
 		}
 		
 		return ResponseEntity.notFound().build();
+	}
+	
+	private OrdemServicoDTO toModel(OrdemServico ordemServico) {
+		return modelMapper.map(ordemServico, OrdemServicoDTO.class);
+	}
+	
+	private List<OrdemServicoDTO> toCollectionDTO(List<OrdemServico> ordensServico) {
+		return ordensServico.stream().map(ordemServico -> toModel(ordemServico)).collect(Collectors.toList());
 	}
 }
